e60e0e30f2564b6826e48e2cfb396f3cc7e047c8
Roy Wiggins <Roy.Wiggins@nyumc.org>
2019-09-24 18:30:26 -0400

    use pysmb for connection

---
+++ serverconnection.py
@@ -0,0 +1,57 @@
+import fs.smbfs
+import socket
+import io
+from pathlib import Path
+from smb.SMBConnection import SMBConnection
+from smb.smb_structs import OperationFailure
+
+class ServerConnection:
+    def __init__(self, server_name):
+        user = 'yarra'
+        password = 'melbourne'
+        self.conn = SMBConnection(user, password, 'yarra-client', server_name)
+        self.conn.connect(server_name)
+
+    def file_exists(self,name):
+        try: 
+            self.conn.getAttributes('YarraServer',name)
+            return True
+        except OperationFailure as e:
+            return False
+
+
+    def task_locked(self,task_name):
+        return self.file_exists(f'{task_name}.lock')
+
+    def lock_task(self, task_name):
+        self.conn.storeFile('YarraServer',f'{task_name}.lock',io.BytesIO(b"lock"))        
+
+    def unlock_task(self, task_name):
+        if self.task_locked(task_name):
+            self.conn.deleteFiles('YarraServer',f'{task_name}.lock')        
+        else:
+            raise Exception(f'{task_name} does not appear to be locked')
+
+    def store(self, file_name, file_obj, *, force=False):
+        path = Path(file_name)
+        if not force and self.file_exists(path.name):
+            raise Exception(f'{path.name} exists on server')
+
+        self.conn.storeFile('YarraServer',path.name,file_obj)
+    def get(self, file_name, file_obj):
+        self.conn.retrieveFile('YarraServer',file_name,file_obj)
+        file_obj.seek(0)
+if __name__ == "__main__":
+    test = ServerConnection('xdglpdcdap002.nyumc.org')
+    test.lock_task('test2')
+
+    try:
+        with io.BytesIO(b'test') as f:
+            test.store('test2.dat',f)
+    finally:
+        test.unlock_task('test2')
+
+    sharedfiles = test.conn.listPath('YarraServer', '/')
+
+    for sharedfile in sharedfiles:
+        print(sharedfile.filename)
---
+++ yarraclient.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python3.7
+import configparser
+from dataclasses import dataclass, field
+from typing import List
+from datetime import datetime
+import socket
+from enum import Enum
+from pathlib import Path
+import os
+from filelock import SoftFileLock
+import shutil
+import io
+
+from serverconnection import ServerConnection
+
+class Priority(Enum):
+     Normal = 1
+     Night = 2
+     High = 3
+
+@dataclass
+class Mode:
+    name: str
+    sort_index: int = 0
+    requires_adj_scans: bool = False
+    requires_acc: bool = False
+    confirmation_mail: str = None
+    tag: str = None
+    required_server_type: str = None
+
+class Server:
+    name: str
+    modes: List[Mode]
+    path: str
+
+
+    def __init__(self, name:str, mnt_path:str):
+        self.name = name
+        self.path = mnt_path
+        self.connection = ServerConnection('xdglpdcdap002.nyumc.org')
+        config = configparser.ConfigParser()
+
+        config_file = io.BytesIO()
+        self.connection.get('YarraModes.cfg',config_file)
+        config_string = config_file.read().decode('UTF-8')
+        
+        config.read_string(config_string)#;(Path(mnt_path,'YarraModes.cfg'))
+
+        mode_info = ( (mode_entry[1], config[mode_entry[1]])  for mode_entry in config.items('Modes') )
+
+        self.modes = { name: Mode(
+                    tag = info.get('tag'),
+                    name = info.get('name'),
+                    confirmation_mail = info.get('confirmationmail'),
+                    requires_adj_scans = info.getboolean('requiresadjscans'),
+                    requires_acc = info.getboolean('requiresacc'),
+                    required_server_type = info.get('requiredservertype'),
+                    sort_index = info.getint('sortindex')
+                ) for name, info in mode_info
+            }
+
+@dataclass
+class TaskData:
+    recon_mode: str
+    email_notification: str
+
+    scan_protocol: str
+
+    recon_name: str
+ 
+    required_server_type: str
+
+    acc_number: str
+    patient_name: str
+
+    scan_file: str
+    scan_file_size: int
+
+    param_value: int = None
+    priority: Priority = Priority.Normal
+    adjustment_files: List[str] = field(default_factory=list)
+    system_name: str = socket.gethostname()
+    system_vendor: str = 'Siemens'
+    system_version: str = 'Unknown'
+    yarra_client: str = 'SAC'
+    client_version: str = '0.1'
+    task_creation_datetime: datetime = field(default_factory=lambda:datetime.now())
+
+    def to_config(self):
+        config = configparser.ConfigParser()
+        config.optionxform = str
+        config['Task'] = dict(
+            ReconName = self.recon_name,
+            ReconMode = self.recon_mode,
+            ScanProtocol = self.scan_protocol,
+            ScanFile = self.scan_file,
+            AdjustmentFilesCount = str(len(self.adjustment_files)) if self.adjustment_files else '0',
+            ParamValue = self.param_value or '0',
+            EMailNotification = self.email_notification
+        )
+
+        config['Information'] = dict(
+                SystemName = self.system_name,
+                ScanFileSize = self.scan_file_size,
+                TaskDate = self.task_creation_datetime.date().isoformat(),
+                TaskTime = self.task_creation_datetime.time().isoformat(),
+                SystemVendor = self.system_vendor,#  "Siemens");
+                SystemVersion = self.system_version,# "Unknown");
+                YarraClient = self.yarra_client,#   "SAC");
+                ClientVersion = self.client_version,# SAC_VERSION);
+                ACC = str(self.acc_number) if self.acc_number is not None else '',
+                PatientName = self.patient_name
+        )
+        
+        io_file = io.StringIO()
+        config.write(io_file)
+        io_file.seek(0)
+        return io_file.getvalue()
+
+class Task():
+    task_name: str 
+    scan_file: Path
+    task_data: TaskData
+    server: Server
+
+    def __init__(self, server, mode_name, scan_file_path, protocol, acc=None, *, email_notifications:list = None, param_value:int=None):
+        self.server = server
+        if (mode_name not in server.modes.keys()):
+            raise Exception(f'Recon mode "{mode_name}" not available on server {server.name}')
+
+        mode = server.modes[mode_name]
+        if mode.requires_adj_scans:
+            raise Exception(f'Recon mode "{mode_name}" requires adjustments, which aren\'t supported yet')
+
+        if not acc and mode.mode_name:
+            raise Exception(f'Recon mode "{mode_name}" requires accession.')
+
+        self.scan_file = Path(scan_file_path)
+        if not os.path.exists(self.scan_file):
+            raise Exception(f'{self.scan_file} not found')
+        self.task_name = self.scan_file.stem
+
+        self.task_data = TaskData(
+            scan_file = self.scan_file.name,
+            scan_file_size = os.path.getsize(self.scan_file),
+            recon_mode = mode_name,
+            email_notification = ','.join(email_notifications) if email_notifications else '',
+            scan_protocol = protocol,
+            recon_name = mode.name,
+            param_value = param_value,
+            required_server_type = None,
+            acc_number = acc,
+            patient_name = 'Joe Bloggs',
+        )
+        # print(self.task_data)
+
+    def submit(self):
+        conn = self.server.connection
+
+        conn.lock_task(self.task_name)
+        try:
+            with open(self.scan_file,'rb') as scan_f:
+                conn.store(f'{self.task_name}.task',io.BytesIO(self.task_data.to_config().encode()))
+                conn.store(f'{self.task_name}.dat',scan_f)
+        finally:
+            conn.unlock_task(self.task_name)
+
+        # with SoftFileLock(Path(self.server.path,self.task_name+'.lock'), timeout=1):
+        #     task_loc = Path(self.server.path,self.task_name+'.task')
+        #     with open(task_loc,'w') as f:
+        #         self.task_data.to_config().write(f)
+        #     try:
+        #         shutil.copy(self.scan_file, Path(self.server.path,self.task_name+'.dat'))
+        #     except Exception as e:
+        #         os.remove(task_loc)
+        #         raise e
