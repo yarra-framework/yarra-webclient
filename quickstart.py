03b650c02bb07073412292563c5c4a00c60318ce
Roy Wiggins <Roy.Wiggins@nyumc.org>
2019-09-30 12:23:01 -0400

    adding user roles...

--- quickstart.py
+++ quickstart.py
@@ -4,9 +4,9 @@
 # - Using class-based configuration (instead of file-based configuration)
 # - Using string-based templates (instead of file-based templates)
 
-from flask import Flask, render_template_string,render_template
+from flask import Flask, render_template_string,render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_user import login_required, UserManager, UserMixin
+from flask_user import login_required, UserManager, UserMixin, roles_required
 
 
 # Class-based application configuration
@@ -25,7 +25,7 @@ class ConfigClass(object):
     USER_ENABLE_EMAIL = False      # Disable email authentication
     USER_ENABLE_USERNAME = True    # Enable username authentication
     USER_REQUIRE_RETYPE_PASSWORD = False    # Simplify register form
-
+    USER_UNAUTHORIZED_ENDPOINT = 'user.logout' # this is not quite right but works for now
 
 def create_app():
     """ Flask application factory """
@@ -53,6 +53,20 @@ def create_app():
         # User information
         first_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
         last_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+        roles = db.relationship('Role', secondary='user_roles')
+
+    # Define the Role data-model
+    class Role(db.Model):
+        __tablename__ = 'roles'
+        id = db.Column(db.Integer(), primary_key=True)
+        name = db.Column(db.String(50), unique=True)
+
+    # Define the UserRoles association table
+    class UserRoles(db.Model):
+        __tablename__ = 'user_roles'
+        id = db.Column(db.Integer(), primary_key=True)
+        user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+        role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
 
     # Create all database tables
     db.create_all()
@@ -64,17 +78,7 @@ def create_app():
     @app.route('/')
     def home_page():
         # String-based templates
-        return render_template_string("""
-            {% extends "flask_user_layout.html" %}
-            {% block content %}
-                <h2>Home page</h2>
-                <p><a href={{ url_for('user.register') }}>Register</a></p>
-                <p><a href={{ url_for('user.login') }}>Sign in</a></p>
-                <p><a href={{ url_for('home_page') }}>Home page</a> (accessible to anyone)</p>
-                <p><a href={{ url_for('member_page') }}>Member page</a> (login required)</p>
-                <p><a href={{ url_for('user.logout') }}>Sign out</a></p>
-            {% endblock %}
-            """)
+        return redirect(url_for('submit_page'))
 
     # The Members page is only accessible to authenticated users via the @login_required decorator
     @app.route('/members')
@@ -94,8 +98,14 @@ def create_app():
             """)
 
     @app.route("/submit")
+    @roles_required('Submitter')
     def submit_page():
         return render_template("test.html", servers=[None])
+
+    @app.route("/admin")
+    def admin():
+        return render_template("admin.html", users=session.query(User).all())
+
 
     return app
 
